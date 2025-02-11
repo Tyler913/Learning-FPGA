f27d39548c061bf87116e0f996aeb2d4f0da06a6
Tyler913 <hongyf050517@outlook.com>
2025-02-10 22:39:05 -0500

    Regular Update

---
+++ 20250208-TouchButton-LED/RTL/TouchButton_LED.v
@@ -0,0 +1,15 @@
+module TouchButton_LED(
+    input wire system_clock,
+    input wire system_reset_n,
+    input wire touch_button,
+
+    output reg led
+);
+
+reg touch_key_1;
+reg touch_key_2;
+reg touch_flag;
+
+
+
+endmodule
