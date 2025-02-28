fcffa818866550a023a2126909911538baa8a7a2
Tyler913 <hongyf050517@outlook.com>
2025-02-28 16:00:49 -0500

    Digital Tube code update

--- 20250221-Dynamic-Digital-Tube/RTL/BCD_8421.v
+++ 20250221-Dynamic-Digital-Tube/RTL/BCD_8421.v
@@ -1,85 +1,74 @@
-module BDC_8421(
-    input wire system_clock,
-    input wire system_reset_n,
-    input wire [19:0] data,
+module  bcd_8421
+(
+    input   wire            sys_clk     ,
+    input   wire            sys_rst_n   ,
+    input   wire    [19:0]  data        ,
 
-    output reg [3:0] one,
-    output reg [3:0] ten,
-    output reg [3:0] hundred,
-    output reg [3:0] thousand,
-    output reg [3:0] ten_thousand,
-    output reg [3:0] hundred_thousand
+    output  reg     [3:0]   unit        ,
+    output  reg     [3:0]   ten         ,
+    output  reg     [3:0]   hun         ,
+    output  reg     [3:0]   tho         ,
+    output  reg     [3:0]   t_tho       ,
+    output  reg     [3:0]   h_hun
 );
 
-reg [4:0] count_shift;
-reg [43:0] data_shift;
-reg shift_flag;
+reg     [4:0]   cnt_shift   ;
+reg     [43:0]  data_shift  ;
+reg             shift_flag  ;
 
+always@(posedge sys_clk or negedge sys_rst_n)
+    if(sys_rst_n == 1'b0)
+        cnt_shift   <=  5'd0;
+    else    if((cnt_shift == 5'd21) && (shift_flag == 1'b1))
+        cnt_shift   <=  5'd0;
+    else    if(shift_flag == 1'b1)
+        cnt_shift   <=  cnt_shift + 1'b1;
+    else
+        cnt_shift   <=  cnt_shift;
 
-always @(posedge system_clock or negedge system_reset_n) begin
-    if (system_reset_n == 1 'b 0) begin
-        count_shift <= 5 'd 0;
-    end
-    else if ((count_shift == 5 'd 21) && (shift_flag == 1 'b 1)) begin
-        count_shift <= 1 'd 0;
-    end
-    else if (shift_flag == 1 'b 1) begin
-        count_shift <= count_shift + 5 'd 1;
-    end
-    else begin
-        count_shift <= count_shift;
-    end
-end
+always@(posedge sys_clk or negedge sys_rst_n)
+    if(sys_rst_n == 1'b0)
+        data_shift  <=  44'b0;
+    else    if(cnt_shift == 5'd0)
+        data_shift  <=  {24'b0,data};
+    else    if((cnt_shift <= 20) && (shift_flag == 1'b0))
+        begin
+            data_shift[23:20]   <=  (data_shift[23:20] > 4) ? (data_shift[23:20] + 2'd3) : (data_shift[23:20]);
+            data_shift[27:24]   <=  (data_shift[27:24] > 4) ? (data_shift[27:24] + 2'd3) : (data_shift[27:24]);
+            data_shift[31:28]   <=  (data_shift[31:28] > 4) ? (data_shift[31:28] + 2'd3) : (data_shift[31:28]);
+            data_shift[35:32]   <=  (data_shift[35:32] > 4) ? (data_shift[35:32] + 2'd3) : (data_shift[35:32]);
+            data_shift[39:36]   <=  (data_shift[39:36] > 4) ? (data_shift[39:36] + 2'd3) : (data_shift[39:36]);
+            data_shift[43:40]   <=  (data_shift[43:40] > 4) ? (data_shift[43:40] + 2'd3) : (data_shift[43:40]);
+        end
+    else    if((cnt_shift <= 20) && (shift_flag == 1'b1))
+        data_shift  <=  data_shift << 1;
+    else
+        data_shift  <=  data_shift;
 
-always @(posedge system_clock or negedge system_reset_n) begin
-    if (system_reset_n == 1 'b 0) begin
-        data_shift <= 44 'd 0;
-    end
-    else if (count_shift == 5 'd 0) begin
-        data_shift <= {24 'd 0, data};
-    end
-    else if ((count_shift <= 5 'd 20) && (shift_flag == 1 'b 0)) begin
-        data_shift[23:20] <= (data_shift[23:20] > 4) ? (data_shift[23:20] + 2 'd 3) : (data_shift[23:20]);
-        data_shift[27:24] <= (data_shift[27:24] > 4) ? (data_shift[27:24] + 2 'd 3) : (data_shift[27:24]);
-        data_shift[31:28] <= (data_shift[31:28] > 4) ? (data_shift[31:28] + 2 'd 3) : (data_shift[31:28]);
-        data_shift[35:32] <= (data_shift[35:32] > 4) ? (data_shift[35:32] + 2 'd 3) : (data_shift[35:32]);
-        data_shift[39:36] <= (data_shift[39:36] > 4) ? (data_shift[39:36] + 2 'd 3) : (data_shift[39:36]);
-        data_shift[43:40] <= (data_shift[43:40] > 4) ? (data_shift[43:40] + 2 'd 3) : (data_shift[43:40]);
-    end
-    else if ((count_shift <= 5 'd 20) && (shift_flag == 1 'b 1)) begin
-        data_shift <= data_shift << 1;
-    end
-    else begin
-        data_shift <= data_shift;
-    end
-end
+always@(posedge sys_clk or negedge sys_rst_n)
+    if(sys_rst_n == 1'b0)
+        shift_flag  <=  1'b0;
+    else
+        shift_flag  <=  ~shift_flag;
 
-always @(posedge system_clock or negedge system_reset_n) begin
-    if (system_reset_n == 1 'b 0) begin
-        shift_flag <= 1 'b 0;
-    end
-    else begin
-        shift_flag <= ~ shift_flag;
-    end
-end
-
-always @(posedge system_clock or negedge system_reset_n) begin
-    if (system_reset_n == 1 'b 0) begin
-        one <= 4 'd 0;
-        ten <= 4 'd 0;
-        hundred <= 4 'd 0;
-        thousand <= 4 'd 0;
-        ten_thousand <= 4 'd 0;
-        hundred_thousand <= 4 'd 0;
-    end
-    else if (count_shift == 5 'd 21) begin
-        one <= data_shift[23:20];
-        ten <= data_shift[27:24];
-        hundred <= data_shift[31:28];
-        thousand <= data_shift[35:32];
-        ten_thousand <= data_shift[39:36];
-        hundred_thousand <= data_shift[43:40];
-    end
-end
+always@(posedge sys_clk or negedge sys_rst_n)
+    if(sys_rst_n == 1'b0)
+        begin
+            unit    <=  4'b0;
+            ten     <=  4'b0;
+            hun     <=  4'b0;
+            tho     <=  4'b0;
+            t_tho   <=  4'b0;
+            h_hun   <=  4'b0;
+        end
+    else    if(cnt_shift == 5'd21)
+        begin
+            unit    <=  data_shift[23:20];
+            ten     <=  data_shift[27:24];
+            hun     <=  data_shift[31:28];
+            tho     <=  data_shift[35:32];
+            t_tho   <=  data_shift[39:36];
+            h_hun   <=  data_shift[43:40];
+        end
 
 endmodule
